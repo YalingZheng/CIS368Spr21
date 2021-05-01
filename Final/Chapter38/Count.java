50edb9a52e49680fbbb209afcf448bf85ed0b694
YalingZheng <yaling.zheng@gmail.com>
2021-05-01 11:23:27 -0400

    Create Count.java

---
+++ Final/Chapter38/Count.java
@@ -0,0 +1,15 @@
+package Chapter38;
+
+public class Count {
+	private int count = 0;
+
+	/** Return count property */
+	public int getCount() {
+		return count;
+	}
+
+	/** Increase count */
+	public void increaseCount() {
+		count++;
+	}
+}
